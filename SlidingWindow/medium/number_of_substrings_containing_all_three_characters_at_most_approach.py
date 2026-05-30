b62461d00cea51eeb08a24a3f5c784be291f5c60
Loopfelloff <abhiyanregmi@gmail.com>
2026-05-31 02:05:01 +0545

    added : solved the previous medium level question using the at most approach

--- SlidingWindow/medium/number_of_substrings_containing_all_three_characters_at_most_approach.py
+++ SlidingWindow/medium/number_of_substrings_containing_all_three_characters_at_most_approach.py
@@ -3,20 +3,23 @@ from collections import defaultdict
 
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        i , j = 0 , 0 
-        n = len(s)
-        dict_ = defaultdict(int)
-        res = (n+1) * n //2
-        while j < n:
-            dict_[s[j]] += 1
-            while len(dict_) >= 3:
-                dict_[s[i]] -= 1
-                if dict_[s[i]] == 0:
-                    del dict_[s[i]]
-                i +=1
-            res -= j - i + 1
-            j += 1 
-        return res
+        def atMost(k):
+            i , j = 0 , 0 
+            n = len(s)
+            dict_ = defaultdict(int)
+            res = 0
+            while j < n:
+                dict_[s[j]] += 1
+                while len(dict_) > k:
+                    dict_[s[i]] -= 1
+                    if dict_[s[i]] == 0:
+                        del dict_[s[i]]
+                    i +=1
+                res += j - i + 1
+                j += 1
+            return res
+
+        return atMost(3) - atMost(2)
 
 
 def main():
